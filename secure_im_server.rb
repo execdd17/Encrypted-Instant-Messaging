3811a70075f14d2cde3a1dab7efdccaa90584a73
Alex <vanadio_alexander@bah.com>
2011-05-02 13:14:23 -0400

    Cleaned up the command line

--- secure_im_server.rb
+++ secure_im_server.rb
@@ -1,21 +1,23 @@
 require 'socket'
 require 'engine'
 
-if ARGV.length != 3 then
-	puts "usage: #{$0} [server port] [client port] [key]"
+if ARGV.length != 5 then
+	puts "usage: #{$0} [local host] [local port] [remote host] [remote port] [key]"
 	exit
 end
 
-$s_port, $c_port, $key = ARGV
-	
+$local_host, $local_port, $remote_host, $remote_port, $key = ARGV
+$debug = false
+
 def start_server(server, port)
 
 	TCPServer.open(server, port) do |server|
 		while client = server.accept
 			begin
-				print(client, " is accepted\n\n")
+				#print(client, " is accepted\n\n") if $debug
+				puts "Inbound Connection Established"
 				cipher_text = client.recv(1000).chomp.split(':')
-				puts "Recieved [#{cipher_text.join(':')}] ciphertext"
+				puts "Recieved [#{cipher_text.join(':')}] ciphertext" if $debug
 				if cipher_text then
 					cipher_text = cipher_text.map { |string_byte| string_byte.to_i.chr }
 					puts decrypt($key, cipher_text.join)
@@ -23,7 +25,7 @@ def start_server(server, port)
 					puts "cipher_text is nil"
 				end
 
-				print(client, " is gone\n")
+				puts "Inbound Connection Terminated"
 			rescue Exception => e
 				puts e.message
 				puts e.backtrace.inspect
@@ -38,7 +40,7 @@ def start_client
 	loop do
 
 		begin 
-			streamSock = TCPSocket.new( "127.0.0.1", $c_port )
+			streamSock = TCPSocket.new( $remote_host, $remote_port )
 		rescue Errno::ECONNREFUSED => e
 			puts "The connection was refused..."
 			puts "Trying again in 5 seconds"
@@ -46,22 +48,22 @@ def start_client
 			next
 		end
 
-		print "Enter Message: "
+		STDOUT.puts "Outbound Connection Established"
 		plain_text = STDIN.gets.strip
 		cipher_text = []
 		encrypt($key, plain_text).bytes { |byte| cipher_text << byte }
 		ct = cipher_text.join(':') + "\n"
-		puts "Sending [#{ct.chomp}]"
+		puts "Sending [#{ct.chomp}]" if $debug
 		streamSock.send(ct,0)
-		puts "Sent!"
+		puts "Sent!" if $debug
 
 		streamSock.close
+		puts "Outbound Connection Terminated"
 	end
 end
 
+##### Main #####
 threads = []
-
-threads << Thread.new { start_server('localhost', ARGV[0]) }
+threads << Thread.new { start_server($local_host, $local_port) }
 threads << Thread.new { start_client }
-
 threads.each { |thread| thread.join }
