86658672368f1521d2478bfdae9fef7575beabee
Alex <vanadio_alexander@bah.com>
2011-05-02 14:10:58 -0400

    Cleaned up the code logic and debugging

--- secure_im_server.rb
+++ secure_im_server.rb
@@ -7,30 +7,35 @@ if ARGV.length != 5 then
 end
 
 $local_host, $local_port, $remote_host, $remote_port, $key = ARGV
-$debug = false
+$debug = true
 
 def start_server(server, port)
 
 	TCPServer.open(server, port) do |server|
 		while client = server.accept
 			begin
-				#print(client, " is accepted\n\n") if $debug
-				puts "Inbound Connection Established"
-				cipher_text = client.recv(1000).chomp.split(':')
-				puts "Recieved [#{cipher_text.join(':')}] ciphertext" if $debug
-				if cipher_text then
-					cipher_text = cipher_text.map { |string_byte| string_byte.to_i.chr }
-					puts decrypt($key, cipher_text.join)
-				else 
-					puts "cipher_text is nil"
+				puts "Inbound Connection Established" if $debug
+				cipher_text = client.recv(1000)	# hard cap on incoming data (buffer)
+				
+				if cipher_text != nil and cipher_text != '' then
+					cipher_text = cipher_text.chomp.split(':')
+
+				# this condition is reached when the user hit just the enter key instead of a msg
+				else
+					puts "User Sent Empty String" if $debug
+					next
 				end
 
-				puts "Inbound Connection Terminated"
+				puts "Recieved [#{cipher_text.join(':')}] ciphertext" if $debug
+				
+				cipher_text = cipher_text.map { |string_byte| string_byte.to_i.chr }
+				puts "#{$remote_host}: #{decrypt($key, cipher_text.join)}"
 			rescue Exception => e
 				puts e.message
 				puts e.backtrace.inspect
 			ensure
 				client.close
+				puts "Inbound Connection Terminated" if $debug
 			end
 		end
 	end
@@ -48,8 +53,15 @@ def start_client
 			next
 		end
 
-		STDOUT.puts "Outbound Connection Established"
+		STDOUT.puts "Outbound Connection Established" if $debug
 		plain_text = STDIN.gets.strip
+
+		if plain_text == '' or plain_text == nil then
+			streamSock.close
+			puts "Outbound Connection Terminated" if $debug
+			next
+		end
+		
 		cipher_text = []
 		encrypt($key, plain_text).bytes { |byte| cipher_text << byte }
 		ct = cipher_text.join(':') + "\n"
@@ -58,7 +70,7 @@ def start_client
 		puts "Sent!" if $debug
 
 		streamSock.close
-		puts "Outbound Connection Terminated"
+		puts "Outbound Connection Terminated" if $debug
 	end
 end
 
